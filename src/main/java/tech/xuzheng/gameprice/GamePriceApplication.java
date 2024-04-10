c6a822071bf2cf3377061e7493fecdf86a73e7ad
Xu Zheng <slowpolitics@outlook.com>
2024-04-10 17:06:54 +0800

    Initial commit

---
+++ src/main/java/tech/xuzheng/gameprice/GamePriceApplication.java
@@ -0,0 +1,13 @@
+package tech.xuzheng.gameprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamePriceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/xuzheng/gameprice/GamePriceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.xuzheng.gameprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
